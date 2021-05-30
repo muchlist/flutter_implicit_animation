864000ffce809684d70c7adbfb2cce55cd13d960
muchlist <whois.muchlis@gmail.com>
2021-05-30 23:01:57 +0800

    init

---
+++ lib/pages/animated_opacity_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class AnimatedOpacityPage extends StatefulWidget {
+  @override
+  createState() => AnimatedOpacityPageState();
+}
+
+class AnimatedOpacityPageState extends State<AnimatedOpacityPage> {
+  var _ovacity = 1.0;
+  void _toggleOpacity() {
+    setState(() {
+      _ovacity = (_ovacity == 1) ? 0.0 : 1.0;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('AnimatedOpacity'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            AnimatedOpacity(
+              opacity: _ovacity,
+              duration: Duration(seconds: 1),
+              curve: Curves.fastOutSlowIn,
+              child: FlutterLogo(size: 200),
+            ),
+            ElevatedButton(
+              child: Text('Fade Logo'),
+              onPressed: _toggleOpacity,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
